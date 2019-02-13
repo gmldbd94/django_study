b689fb50deb76777a4dfafc73a4a5e806aa9357d
heeyung <gmldbd@naver.com>
2019-02-13 19:54:46 +0900

    index url

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from polls import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
+    path('', views.IndexView.as_view()),
 ]
